99c2f17e1f9bca395dbfad49aefe4fec879b5a7c
Kaixianxian1214 <1678092258@qq.com>
2021-11-11 11:28:07 +0800

    "mac mini"

--- src/main/java/com/example/webdemo/controller/TestController.java
+++ src/main/java/com/example/webdemo/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.example.webdemo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +12,30 @@ public class TestController {
     public String Hello(){
         String s = "Hello, world!";
         return s;
+    }
+
+//    @RequestMapping("/index.html")
+//    public String index(){
+//        return "index Page";
+//    }
+
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public String getUser(){
+        return "GET";
+    }
+
+    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    public String postUser(){
+        return "POST";
+    }
+
+    @RequestMapping(value = "/user", method = RequestMethod.DELETE)
+    public  String deleteUser(){
+        return "DELETE";
+    }
+
+    @RequestMapping(value = "/user", method =RequestMethod.PUT)
+    public String putUser(){
+        return "PUT";
     }
 }
