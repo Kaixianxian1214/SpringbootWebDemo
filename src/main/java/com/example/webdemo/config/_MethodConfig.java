7d305102d2be242607d13fc66cafcbe9f030256f
Kaixianxian1214 <1678092258@qq.com>
2021-11-11 16:25:13 +0800

    mac mini

---
+++ src/main/java/com/example/webdemo/config/_MethodConfig.java
@@ -0,0 +1,16 @@
+package com.example.webdemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+@Configuration(proxyBeanMethods = false)
+public class _MethodConfig {
+
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter(){
+        HiddenHttpMethodFilter methodFilter = new HiddenHttpMethodFilter();
+        methodFilter.setMethodParam("_m");
+        return methodFilter;
+    }
+}
